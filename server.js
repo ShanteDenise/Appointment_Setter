835017141c614d5ad2d06ae04ba9da659da3b56f
Shante Austin <shante.austin19@gmail.com>
2019-02-01 14:23:57 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.connect(process.env.MONGODB_URI); 
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully')
+})
+
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+}) 
+
+app.use(bodyParser.json());
+app.get('/', (req,res) => {
+  res.send('Hello world!')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
+})
