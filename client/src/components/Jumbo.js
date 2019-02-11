07d1746d67e39496f8614720c6dc6d8b20f05692
Shante Austin <shante.austin19@gmail.com>
2019-02-10 23:31:49 -0500

    Added additional Styling

--- client/src/components/Jumbo.js
+++ client/src/components/Jumbo.js
@@ -11,7 +11,7 @@ export default class Jumbo extends Component {
         <h1 className="display-3">Schedule Your Appointment</h1>
         <p className="lead">We focus on continually improving our products to create faster vehicle transactions, enabling consumers to have a seamless online-to-offline experience. </p>
         <hr className="my-2" />
-        <p>Click on a Time Below to Book an Appointment or Click the Button to Learn more about how Cox Automotive solutions can take every aspect of your business further, faster. </p>
+        <p>Click on a Time below to Book an Appointment or Click the Button to Learn more about how Cox Automotive solutions can take every aspect of your business further, faster. </p>
         <p className="lead">
           <Button color="primary">Learn More</Button>
         </p>
