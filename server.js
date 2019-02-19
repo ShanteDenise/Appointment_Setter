57607c27ee90974345e88391c6ff37172cb1880f
Shante Austin <shante.austin19@gmail.com>
2019-02-19 18:38:38 -0500

    heroku fix

--- server.js
+++ server.js
@@ -52,7 +52,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override')
 
@@ -68,7 +67,6 @@ app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.use('/', indexRouter);
