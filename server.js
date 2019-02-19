f19172f5ef22ca1d423e959b3eac57c22afa2d50
Shante Austin <shante.austin19@gmail.com>
2019-02-19 18:31:20 -0500

    heroku fix

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', 
 });
 
 // Configuration
-app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 
