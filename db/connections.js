d6c04054050934498488b7763909e35bda86ade5
Shante Austin <shante.austin19@gmail.com>
2019-02-19 14:35:15 -0500

    error fix

--- db/connections.js
+++ db/connections.js
@@ -2,31 +2,31 @@
 require('dotenv').config();
 
 // Connect to database
-const mongoose = require('mongoose');
-
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-  }
-  else {
-    mongoose.connect('mongodb://localhost/appointment-setter');
-  }
-  mongoose.connection.on('error', function(err) {
-    console.error('MongoDB connection error: ' + err);
-    process.exit(-1);
-    }
-  );
-  mongoose.connection.once('open', function() {
-    console.log("Mongoose has connected to MongoDB!");
-  });
-
 // const mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-//     console.log('Connected to MongoDB')
-// }) 
-// // If the connection throws an error
-// mongoose.connection.on('error', (err) => {
-//     console.log('Mongoose default connection error: ' + err);
-//   }) 
+
+// if (process.env.MONGODB_URI) {
+//     mongoose.connect(process.env.MONGODB_URI);
+//   }
+//   else {
+//     mongoose.connect('mongodb://localhost/appointment-setter');
+//   }
+//   mongoose.connection.on('error', function(err) {
+//     console.error('MongoDB connection error: ' + err);
+//     process.exit(-1);
+//     }
+//   );
+//   mongoose.connection.once('open', function() {
+//     console.log("Mongoose has connected to MongoDB!");
+//   });
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB')
+}) 
+// If the connection throws an error
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ' + err);
+  }) 
 
 
 module.exports = mongoose
--- server.js
+++ server.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const routes = require('./routes/index')
+const mongoose = require('mongoose');
+
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
 
-app.use('/', routes)
+// app.use('/', routes)
 
-app.use(express.static(__dirname + '/client/build/'))
+// app.use(express.static(__dirname + '/client/build/'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/client/build/index.html')
+// })
 
-const PORT = process.env.PORT || port
+// const PORT = process.env.PORT || 3001
+
+// app.listen(PORT, () => {
+//   console.log("Magic happening on port " + PORT);
+// });
+
+
+const PORT = process.env.PORT || 8080; // Step 1
+
+
+// Step 2
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
+    useNewUrlParser: true
+});
+
+// Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes);
+
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
 
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+    log(`Server is starting at PORT: ${PORT}`);
 });
