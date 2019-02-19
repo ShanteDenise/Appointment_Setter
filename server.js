3a9a4a8527f6db4f4efe4ca89ecd5a12e7719ac5
Shante Austin <shante.austin19@gmail.com>
2019-02-19 12:16:26 -0500

    heroku fix

--- server.js
+++ server.js
@@ -14,12 +14,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
 })
 
+const PORT = process.env.PORT || port
 
-const PORT = process.env.PORT || 3001;
-
-// app.listen(PORT, () => {
-//   console.log("Magic happening on port " + PORT);
-// });
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
 });
