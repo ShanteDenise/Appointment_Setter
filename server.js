28d9604ac0e8bd059738851ccd0f9bf55abc2658
Shante Austin <shante.austin19@gmail.com>
2019-02-19 18:54:11 -0500

    heroku fix #35

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var methodOverride = require('method-override')
 
 var indexRouter = require('./routes/index');
 
@@ -36,7 +35,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
