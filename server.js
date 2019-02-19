31a656a2eea77b34fe91cedab7da8aa686d30447
Shante Austin <shante.austin19@gmail.com>
2019-02-19 18:51:34 -0500

    heroku fix #30

--- server.js
+++ server.js
@@ -1,21 +1,70 @@
-const express = require('express');
-const routes = require('./routes/index')
+// const express = require('express');
+// const routes = require('./routes/index')
 
-const app = express();
+// const app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
 
-app.use('/', routes)
+// app.use('/', routes)
 
-app.use(express.static(__dirname + '/client/build/'))
+// app.use(express.static(__dirname + '/client/build/'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/client/build/index.html')
+// })
+
+
+// const PORT = process.env.PORT || 3001;
+
+// app.listen(PORT, () => {
+//   console.log("Magic happening on port " + PORT);
+// });
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var methodOverride = require('method-override')
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(methodOverride('_method'))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT: ${PORT}`)
 })
 
-const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
-});
+
+module.exports = app;
