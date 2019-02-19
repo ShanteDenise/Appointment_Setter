57c31346876c6cd92cce6c11e4a651f30edb636b
Shante Austin <shante.austin19@gmail.com>
2019-02-19 18:36:47 -0500

    heroku fix

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const routes = require('./routes/index')
-const mongoose = require('mongoose');
+// const express = require('express');
+// const routes = require('./routes/index')
+// const mongoose = require('mongoose');
 
 
-const app = express();
+// const app = express();
+// app.use(logger('dev'));
 
 // app.use(express.urlencoded({ extended: true }))
 // app.use(express.json())
@@ -23,27 +24,75 @@ const app = express();
 // });
 
 
-const PORT = process.env.PORT || 8080; // Step 1
+// const PORT = process.env.PORT || 8080; // Step 1
 
 
-// Step 2
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
-    useNewUrlParser: true
+// // Step 2
+// mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
+//     useNewUrlParser: true
+// });
+
+// // Configuration
+
+// app.use('/', routes);
+
+// // Step 3
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static( 'client/build' ));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+//     });
+// }
+
+// app.listen(PORT, () => {
+//     log(`Server is starting at PORT: ${PORT}`);
+// });
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var methodOverride = require('method-override')
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(methodOverride('_method'))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// Configuration
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.use('/', routes);
-
-// Step 3
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-    });
-}
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    log(`Server is starting at PORT: ${PORT}`);
-});
+  console.log(`Server is listening on PORT: ${PORT}`)
+})
+
+
+
+module.exports = app;
