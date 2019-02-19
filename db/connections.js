9f4e6fe7982de23b149502c4976bb1c9aa61c37b
Shante Austin <shante.austin19@gmail.com>
2019-02-19 12:23:43 -0500

    Updated DB connection code to use MongoLab when in Production

--- db/connections.js
+++ db/connections.js
@@ -1,14 +1,30 @@
 //Connection to a database
 require('dotenv').config();
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log('Connected to MongoDB')
-}) 
-// If the connection throws an error
-mongoose.connection.on('error', (err) => {
-    console.log('Mongoose default connection error: ' + err);
-  }) 
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/appointment-setter');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
+
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI).then(() => {
+//     console.log('Connected to MongoDB')
+// }) 
+// // If the connection throws an error
+// mongoose.connection.on('error', (err) => {
+//     console.log('Mongoose default connection error: ' + err);
+//   }) 
 
 
 module.exports = mongoose
