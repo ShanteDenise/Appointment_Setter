240352c94c6ec4211f746b6c6a3d4253ca00f1f9
Shante Austin <shante.austin19@gmail.com>
2019-02-19 18:55:46 -0500

    heroku fix #35

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine');
 
 app.use(logger('dev'));
 app.use(express.json());
