9eafc1686e0ab9126a4ea133400f365dfd9abc51
Shante Austin <shante.austin19@gmail.com>
2019-02-19 12:06:35 -0500

    Made changes to server

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+// app.listen(PORT, () => {
+//   console.log("Magic happening on port " + PORT);
+// });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
